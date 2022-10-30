23e89c6265be53d52d42219a624950476209e208
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-10-30 20:02:13 +0400

    Домашнее задание 2. Задание 1 - 2.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 import homework1.Product;
 import homework1.ProductList;
+import homework2.Recipe;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
 
 public class Main {
 
@@ -10,7 +16,29 @@ public class Main {
         productList.addProduct(banana);
         productList.addProduct(fish);
         System.out.println(productList);
-        Product banana2 = new Product("банан", 31.0f, 7);
-        productList.addProduct(banana2);
+
+        //Product banana2 = new Product("банан", 31.0f, 7);
+        //productList.addProduct(banana2);
+
+        System.out.println();
+
+        Recipe recipe = new Recipe();
+    }
+
+    private static void numbersSetRemoveEven() {
+        Set<Integer> numbers = new HashSet<>();
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            numbers.add(random.nextInt(1000));
+        }
+        System.out.println(numbers);
+        Iterator<Integer> iterator = numbers.iterator();
+        while (iterator.hasNext()) {
+            Integer next = iterator.next();
+            if (next % 2 != 0) {
+                iterator.remove();
+            }
+        }
+        System.out.println(numbers);
     }
 }
